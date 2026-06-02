980794d862416cc34a1cbeb3b982276dcb12ff71
victoryip4s-hub <victoryip4s@gmail.com>
2026-06-01 23:07:48 -0400

    Add Tailwind CSS configuration

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#6366f1',
+        secondary: '#8b5cf6',
+      },
+    },
+  },
+  plugins: [],
+}
